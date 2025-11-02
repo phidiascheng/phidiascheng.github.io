4ded526e78026df7f242e7c312d100dbe95ee5ae
phidiascheng <phidiascheng@qq.com>
2025-11-02 22:18:32 +0800

    t

--- components/WorkB.js
+++ components/WorkB.js
@@ -12,95 +12,146 @@ import { loadLessStyle, removeElementById } from './utility.js';
 function Vocabulary(){
 
     const lessStyle = /* css */`
-        .work_main{
-            max-width: 100%;
-            text-align: center; 
-        }
-/*         @media screen and (max-width:840px) {
-            .work_main .pdfbox {width: 95%; aspect-ratio: 3/2.2;}
-        }
-        @media screen and (min-width:840px) {
-            .work_main .pdfbox {width: 800px; aspect-ratio: 3/2.2;}
-        }  */
         @font-face {
             font-family: "Courier New";
             font-style: normal;
             font-weight: 100;
             src: url('./assets/fonts/CourierNew.ttf') format('truetype');
         }
-
-        #header{
-            display: flex;
-            justify-content: left;
-            height: 20px;
-            align-items: center;
-        }
-
-        #wordBox {
-            display: flex;
-            justify-content: center;
-            align-items: start;
-            height: calc(100vh - 53px - 20px - 5px);
-            font-family: 'Times New Roman', Times, serif;
-        }
-        #word {
-            -webkit-user-select: none;
-            height: calc((100vh - 53px - 20px - 5px)*0.3);
-            font-size: calc((100vh - 20px - 20px - 5px)*0.28);
-            display: flex;
-            justify-content: center;
-            align-items: end;
-            color: #d62626;
-            font-family:  'Times New Roman', Times, serif;
-            margin-bottom: 3vh;
-        }
-        .exp {
-            -webkit-user-select: none;
-            height: calc((100vh - 53px - 20px - 5px)*0.13);
-            font-size: calc((100vh - 53px - 20px - 5px)*0.12);
-            display: flex;
-            justify-content: left;
-            align-items: center;
-            color: blue;
-            border: solid;
-            border-width: 0.5vh;
-            border-color: white;
-            border-radius: 1vh;
-        }
-        .chosed{
-            background-color: rgb(212, 223, 183);
-            border-color: red;
-        }
-        .correct{
-            color: red;
-            border-color: red;
-            background-color: antiquewhite;
-        }
-        .wrong{
-            color: black;
-        }
-        .optionLabel{
-            font-family: 'Courier New', "SimSun";
-            position: relative;
-            top: 0.3vh;
-            width: 1em;
-        }
-        .optionText{
-            font-family: "SimSun";
-            position: relative;
-            top: 0.2vh;
+        .work_main{
+            display: block;
+            text-align: center; 
+            margin-top: 13px;
+            #header{
+                display: flex;
+                justify-content: left;
+                height: 20px;
+                align-items: center;
+                #headerLabel{
+                    text-align: left;
+                    width: 5em;
+                    font-size: 8.5pt;
+                    font-family: 'arial';
+                }
+            }
+            #wordBox {
+                display: block;
+                height: calc(100vh - 53px - 20px - 5px);
+                font-family: 'Times New Roman', Times, serif;
+                #word {
+                    -webkit-user-select: none;
+                    height: calc((100vh - 53px - 20px - 5px)*0.3);
+                    font-size: calc((100vh - 20px - 20px - 5px)*0.28);
+                    display: flex;
+                    justify-content: center;
+                    align-items: end;
+                    margin-bottom: 3vh;
+                    overflow: hidden;
+                    white-space: nowrap;
+                    #wordText{
+                        color: #d62626;
+                        position:relative;
+                        top: 0em;
+                        font-family:  'Times New Roman', Times, serif;
+                    }
+                    .wordScroll {
+                        display: inline-block;
+                        animation: wordTextScroll 20s linear infinite;
+                    }
+                    @keyframes wordTextScroll {
+                        0% { transform: translateX(50%); }
+                        100% { transform: translateX(-100%); }
+                    }
+                }
+                .exp {
+                    -webkit-user-select: none;
+                    height: calc((100vh - 53px - 20px - 5px)*0.13);
+                    font-size: calc((100vh - 53px - 20px - 5px)*0.12);
+                    display: flex;
+                    justify-content: left;
+                    color: blue;
+                    border: solid;
+                    border-width: 0.5vh;
+                    border-color: white;
+                    border-radius: 1vh;
+                    margin: 0 1em;
+                    @media (max-width: 1200px){
+                        margin: 0;
+                    }
+                    .optionLabel{
+                        text-align: center;
+                        min-width: 1em;
+                        .optLabel{
+                            font-family: 'Courier New', "SimSun";
+                            position: relative;
+                            top: -3vh;
+                        }
+                    }
+                    .optionText{
+                        flex-grow: 0;
+                        text-align: left;
+                        overflow: hidden;
+                        white-space: nowrap;
+                        .optText{
+                            position: relative;
+                            top: -2vh;
+                            font-family: "SimSun";
+                            font-size: calc((100vh - 53px - 20px - 5px)*0.12);
+                        }
+                        .expScroll {
+                            display: inline-block;
+                            animation: expTextScroll 20s linear infinite;
+                        }
+                        @keyframes expTextScroll {
+                            0% { transform: translateX(0vw); }
+                            100% { transform: translateX(-100%); }
+                        }
+                    }
+                    .optionTextFade{
+                        -webkit-mask-image: linear-gradient(90deg, transparent, black 5%);
+/*                         mask-image: linear-gradient(90deg, transparent, black 5%); */
+                    }
+                    .optionMark{
+                        font-family: "SimSun";
+                        font-size: calc((100vh - 53px - 20px - 5px)*0.16);
+                        position: relative;
+                        color: red;
+                        display: inline-block;
+                        text-align: center;
+                        width: 0.5em;
+                        top: -3.2vh;
+                    }
+                }
+                .chosed{
+                    background-color: rgb(212, 223, 183);
+                    border-color: red;
+                }
+                .correct{
+                    color: red;
+                    border-color: red;
+                    background-color: antiquewhite;
+                }
+                .wrong{
+                    color: black;
+                }
+            }
+        #statistics{
+            text-align: left;
+            display: none; 
+            padding: 0 3vw;
+            @media (max-width:  1200px){
+                padding: 0;
+            }
         }
         #progressBar{
             height:5px; 
             width:90%; 
             background-color: gray;
         }
-        #statistics{
-            text-align: left;
-            display: none; 
-            width: 80%;
         }
-    `;/* css */
+
+
+`;/* css */
 
 
 
@@ -111,11 +162,13 @@ function Vocabulary(){
     useEffect( () => {
         document.addEventListener("keydown", keydownListerner);
         document.addEventListener("wheel", wheelListerner);
+        window.addEventListener("resize", resizeListerner);
 
         return ( () => {
             removeElementById('stywVocab')
             document.removeEventListener("keydown", keydownListerner);
             document.removeEventListener("wheel", wheelListerner);
+            window.removeEventListener("resize", resizeListerner);
         })
     },[]);
 
@@ -132,7 +185,6 @@ function Vocabulary(){
 
     const options = ['A','B', 'C', 'D']
     let fileName = ''
-
 
 
     function readFile(){
@@ -154,8 +206,6 @@ function Vocabulary(){
                 isFileReady = true;
                 currentIndex = 0;
 
-                document.getElementById("statistics").style.display = 'none'
-                document.getElementById("mainBoard").style.display = 'block'
                 display()
             };
             reader.readAsText(file);
@@ -174,9 +224,24 @@ function Vocabulary(){
     /*-----------------------------------------------------------------*/
 
     function moveTo(targ){
+        checkTextOverFlow(chosedAnswer,targ)
         document.getElementById(chosedAnswer).classList.remove('chosed')
         chosedAnswer = targ
         document.getElementById(chosedAnswer).classList.add('chosed')
+    }
+
+    /*-----------------------------------------------------------------*/
+
+    function checkTextOverFlow(orig, targ){
+        document.getElementById("exp"+orig+"Text").classList = 'optText'
+        document.getElementById("optionText"+orig).classList = "optionText"
+        let ele = document.getElementById("optionText"+targ)
+        if( !document.getElementById("exp"+targ+"Text").classList.contains("expScroll") ){
+            if (ele.scrollWidth > ele.clientWidth){
+                document.getElementById("exp"+targ+"Text").classList.add("expScroll")
+                ele.classList.add("optionTextFade")
+            }
+        }
     }
 
     /*-----------------------------------------------------------------*/
@@ -194,20 +259,54 @@ function Vocabulary(){
     }
 
 
-    function playAudio(){
-        const mp3Url = "http://dict.youdao.com/dictvoice?type=0&audio="+words[currentIndex][0];
-        const audioPlayer = document.getElementById('audioPlayer');
-        audioPlayer.src = mp3Url;
-        audioPlayer.load();
-        audioPlayer.play().catch(error => {
-            // console.error('播放音频时出错:', error);
-        });
+    function playAudio(type){
+        if(type == 'word'){
+            const mp3Url = "http://dict.youdao.com/dictvoice?type=0&audio="+words[currentIndex][0];
+            const audioPlayer = document.getElementById('audioPlayer');
+            audioPlayer.src = mp3Url;
+            audioPlayer.load();
+            audioPlayer.play().catch(error => {
+                // console.error('播放音频时出错:', error);
+            });
+        }
+        else if (type == 'wrong'){
+            const music = new Audio('./data/audio/wrong.m4a');
+            music.play();
+        }
+        else if (type == 'right'){
+            const music = new Audio('./data/audio/right.m4a');
+            music.play();
+        }
     }
 
 
+    function resetState(){
+
+        document.getElementById("statistics").style.display = 'none'
+        document.getElementById("wordBox").style.display = 'block'
+        document.getElementById("progressBar").style.display = 'block'
+
+        document.getElementById("wordText").classList = ''
+        document.getElementById("wordText").innerText = ''
+        for ( let i=0; i<4; i++){
+            document.getElementById("exp"+i+"Label").innerText = ''
+            document.getElementById("exp"+i+"Text").innerText = ''
+            document.getElementById("exp"+i+"Mark").innerText = ''
+            document.getElementById("optionText"+i).classList = 'optionText'
+            document.getElementById(i).classList = 'exp'
+        }
+    }
+
 
     function display(){
-        document.getElementById("word").textContent = words[currentIndex][0];
+
+        resetState()
+        document.getElementById("wordText").innerText = words[currentIndex][0];
+        let ele = document.getElementById("word")
+        if(ele.scrollWidth > ele.clientWidth){
+            document.getElementById("wordText").classList.add("wordScroll")
+        }
+
         let orderArray = [...Array(words.length).keys()]
         orderArray.splice(currentIndex, 1);
         shuffleArray(orderArray)
@@ -218,12 +317,12 @@ function Vocabulary(){
         chosedAnswer = 0
 
         for (let i = 0; i < optionNumber; i++){
-            document.getElementById(i).classList = 'exp'
-            document.getElementById(i).innerHTML = `<span class="optionLabel">${options[i]}</span>`+`<span class="optionText">${words[optionsIndices[i]][1]}</span>`;
+            document.getElementById("exp"+i+"Label").innerText = options[i]
+            document.getElementById("exp"+i+"Text").innerText = words[optionsIndices[i]][1]
         }
         document.getElementById(chosedAnswer).classList.add('chosed')
         document.getElementById("progressBar").style.width = (currentIndex+1)/words.length * 100 + "%"
-        playAudio()
+        playAudio('word')
     }
 
 
@@ -245,8 +344,9 @@ function Vocabulary(){
                 }
             }
             else {
-                if ( event.code == "Space" )
+                if ( event.code == "Space" ){
                     startNext()
+                }
             }
 
             if ( event.altKey && event.code == 'KeyS'){
@@ -271,12 +371,21 @@ function Vocabulary(){
         }
     }
 
+    function resizeListerner(){
+        console.log('resized')
+        display()
+    }
+
+
+
     function optionClick(){
         if( isFileReady ){
-            if( !isAnswerChecked )
+            if( !isAnswerChecked ){
                 checkAnswer()
-            else
+            }
+            else{
                 startNext()
+            }
         }
     }
 
@@ -290,16 +399,18 @@ function Vocabulary(){
     function checkAnswer(){
         if (chosedAnswer == correctAnswer){
             words[currentIndex][2] = String( +words[currentIndex][2] + 1 )
-            words[currentIndex][4] += 1
-            document.getElementById(chosedAnswer).innerHTML += `<span style="color: red">✓</span>`
+            words[currentIndex][4] = +(words[currentIndex][4]) + 1
+            document.getElementById("exp"+chosedAnswer+"Mark").innerText += "✓"
             document.getElementById(chosedAnswer).classList.add('correct')
+            playAudio('right')
         } 
         else {
             words[currentIndex][3] = String( +words[currentIndex][3] + 1 )
             words[currentIndex][4] = 0
-            document.getElementById(chosedAnswer).innerHTML += `<span style="color: red">✗</span>`
+            document.getElementById("exp"+chosedAnswer+"Mark").innerText = "✗"
             document.getElementById(chosedAnswer).classList.add('wrong')
             document.getElementById(correctAnswer).classList.add('correct')
+            playAudio('wrong')
         }
         isAnswerChecked = true
     }
@@ -308,39 +419,28 @@ function Vocabulary(){
     /*-----------------------------------------------------------------*/
 
     function dispStatistics(){
-        document.getElementById("word").innerHTML = '';
-        for (let i = 0; i < optionNumber; i++ ){
-            document.getElementById(i).textContent = '';
-            document.getElementById(i).classList = 'exp';
-        }
 
         const currentInd = isAnswerChecked ? currentIndex + 1 : currentIndex
 
-        document.getElementById("mainBoard").style.display = 'none'
+        document.getElementById("wordBox").style.display = 'none'
         document.getElementById("statistics").style.display = 'block'
+        document.getElementById("progressBar").style.display = 'none'
         let fontSize = 7
         document.getElementById("statistics").innerHTML = `<span style='font-size: ${fontSize}vh; font-family:"SimSun"'>错误的单词有：</span><br>`
         for( let i = 0; i < currentInd; i++){
-            if( words[i][4] == 0 )
+            if( words[i][4] == 0 ){
                 document.getElementById("statistics").innerHTML += `<span style="font-size: ${fontSize}vh; color: red; font-family:'Times New Roman'">${words[i][0]}、</span>`
-            if (i % 5 == 4)
-                document.getElementById("statistics").innerHTML += '<br>'
+            }
         }
         document.getElementById("statistics").innerHTML += `<br><br><br><br><span style='font-size: ${fontSize}vh; font-family:"SimSun"'>正确的单词有：</span><br>`
         for( let i = 0; i < currentInd; i++){
-            if( words[i][4] == 1 )
+            if( words[i][4] > 0 ){
                 document.getElementById("statistics").innerHTML += `<span style="font-size: ${fontSize}vh; color: green;font-family:'Times New Roman'">${words[i][0]}、</span>`
-            if (i % 5 == 4)
-                document.getElementById("statistics").innerHTML += '<br>'
+            }
         }
         document.getElementById("statistics").innerHTML += `<br><br><br><br><span style='font-size: ${fontSize}vh; font-family:"SimSun"'>没做的单词有：</span><br>`
-        console.log(currentIndex)
-        console.log(words.length)
         for( let i = currentInd; i < words.length; i++){
-            console.log(i)
             document.getElementById("statistics").innerHTML += `<span style="font-size: 4vh; color: black; font-family:'Times New Roman'">${words[i][0]}、</span>`
-            if (i % 5 == 4)
-                document.getElementById("statistics").innerHTML += '<br>'
         }
     }
 
@@ -367,35 +467,45 @@ function Vocabulary(){
     }
 
 
+
+
+
+
+    const expList = [0,1,2,3].map(i => 
+        <div id={i} class="exp" onMouseMove={()=>{optionMouseMove(i)}} onClick={optionClick}>
+            <div class="optionLabel">
+                <span id={"exp"+i+"Label"} class="optLabel">
+                </span>
+            </div>
+            <div id={"optionText"+i} class="optionText">
+                <span id={"exp"+i+"Text"} class="optText">
+                </span>
+            </div>
+            <span id={"exp"+i+"Mark"} class="optionMark">
+            </span>
+        </div>
+    )
+
     return (
     <div class="card-display">
-
-        <a>
-            <h2 className="card-title"></h2>
-        </a>
         <div className="work_main">
-
             <div id="header">
-                <span style={{fontSize: "8.5pt", fontFamily: "arial"}}>读取词库&nbsp;&nbsp;</span>
+                <span id="headerLabel">读取词库</span>
                 <input type="file" id="fileInput" accept=".txt" onChange={readFile} />
             </div>
             
             <div id="wordBox">
-                <div id="mainBoard" style={{width: "80%"}} >
-                    <div id="word"> </div>
-                    <div id="0" class="exp" onMouseMove={()=> {optionMouseMove(0)}} onClick={optionClick}> </div>
-                    <div id="1" class="exp" onMouseMove={()=> {optionMouseMove(1)}} onClick={optionClick}> </div>
-                    <div id="2" class="exp" onMouseMove={()=> {optionMouseMove(2)}} onClick={optionClick}> </div>
-                    <div id="3" class="exp" onMouseMove={()=> {optionMouseMove(3)}} onClick={optionClick}> </div>
+                <div id="word">
+                    <span id="wordText"></span>
                 </div>
+
+                { expList }
+                
                 <div><audio id="audioPlayer"></audio></div>
-                <div id="statistics"></div>
             </div>
+            <div id="statistics"></div>
             <div id="progressBar"></div>
-
-
         </div>
-
     </div>    
     )
 }
